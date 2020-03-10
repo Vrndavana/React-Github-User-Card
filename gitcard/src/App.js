249d038b8f7873b9b1294bf17b4b5ce79a790716
Vrndavana <Vrmdavama.th@gmail.com>
2020-03-10 12:46:59 -0700

    Class Component Set Up w State

---
+++ gitcard/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+class App extends React.Component {
+  state = {
+    user: [],
+    followers:[]
+  };
+
+
+  render() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+
+
+
+
+      </header>
+    </div>
+  );}
+}
+
+export default App;
